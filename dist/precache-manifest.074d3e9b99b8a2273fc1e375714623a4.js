c644c9679a25cffde359e4a759036b3ef84fdc40
sherlockkid7 <18381072971@163.com>
2020-05-02 14:27:36 +0800

    update

---
+++ dist/precache-manifest.074d3e9b99b8a2273fc1e375714623a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa9b8b5304ea4e958c3",
+    "url": "css/app.3382760d.css"
+  },
+  {
+    "revision": "7e33c6bb131052db31d3",
+    "url": "css/chunk-vendors.42938589.css"
+  },
+  {
+    "revision": "5626127020876227a69b00da5c773d13",
+    "url": "img/iconfont.56261270.svg"
+  },
+  {
+    "revision": "8ba8726333d0e97426d5e64389bf1248",
+    "url": "index.html"
+  },
+  {
+    "revision": "daa9b8b5304ea4e958c3",
+    "url": "js/app.1b80c4e7.js"
+  },
+  {
+    "revision": "7e33c6bb131052db31d3",
+    "url": "js/chunk-vendors.2eec7d0b.js"
+  },
+  {
+    "revision": "bdba5fdb28c61642ec06dec940470a8b",
+    "url": "manifest.json"
+  }
+]);
